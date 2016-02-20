7a0ff3a09031386e189d09da1e3b26ded43b6e43
Julia Nocera <giulz42@gmail.com>
2016-02-20 13:20:59 -0800

    adds a form input for new comments on html file with new template to be added, adds an event handler for form that adds data to mongo db

--- first_app.js
+++ first_app.js
@@ -9,7 +9,26 @@ if (Meteor.isClient) {
       return Comments.find();
                           // cursor is like a reg js obj so you can call functions on it
     }
-  })
+  });
+
+  Template.CommentAdd.events({   // how to set an event
+    'submit form': function(e, tmpl) {// handler & selector  with 'e' like jQuery and 'tmpl' = object that reps instance of template on page
+      e.preventDefault();  // wont submit
+
+      var formEl = tmpl.find('form');
+      var commentEl = tmpl.find("[name=comment]");
+      var comment = commentEl.value;
+
+      Comments.insert({
+        login: 'giulz42', 
+        timestamp: new Date,
+        room: 'main',
+        comment: comment   // adds comment from above
+      });
+
+      formEl.reset();
+    }
+  });
 }
 
 if (Meteor.isServer) {
